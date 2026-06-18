0681a46ef517a39bafa28dec785d301bfb4d9994
Polasam Lakshmana  sai <polasamlakshmanasai@gmail.com>
2026-06-18 14:54:31 +0530

    Fix Vercel deployment

--- main.py
+++ main.py
@@ -273,9 +273,6 @@ def view_users():
 # ==========================
 
 if __name__ == "__main__":
+    app.run(debug=True)
 
-    app.run(
-        debug=True,
-        host="0.0.0.0",
-        port=5000
-    )
+app = app
